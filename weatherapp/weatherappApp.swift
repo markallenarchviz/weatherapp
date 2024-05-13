c33ca046464f8b46cc774902cb50fd173e96e06b
Mark Allen <markallenarchviz@gmail.com>
2024-05-13 18:17:46 -0300

    init project

---
+++ weatherapp/weatherappApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappApp.swift
+//  weatherapp
+//
+//  Created by mark on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
